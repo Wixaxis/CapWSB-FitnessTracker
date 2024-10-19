917f73b3ad8f1a56075c2335f14e2057bde409a3
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2024-10-19 12:46:36 +0200

    endpoint /v1/users/email  -> copleted controller, with use exists methods

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,6 +1,7 @@
 package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
+import com.capgemini.wsb.fitnesstracker.exception.api.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,13 @@ class UserController {
                           .map(userMapper::toDto)
                           .toList();
     }
+    @GetMapping("/email")
+    public List<UserDto> getUserByEmail(@RequestParam String email) {
+        return userService.getUserByEmail(email)
+                .map(user -> List.of(userMapper.toDto(user)))
+                .orElseThrow(() -> new NotFoundException("User not found with email: " + email));
+    }
+
 
     @PostMapping
     public User addUser(@RequestBody UserDto userDto) throws InterruptedException {
