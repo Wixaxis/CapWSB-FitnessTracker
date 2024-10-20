c0f4e4a320c25a61e5c1e3d87eda8478d49994d7
Wiktor Rojecki <wixaxis@proton.me>
2024-10-20 15:55:04 +0200

    updates for user controller

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,17 +1,19 @@
 package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
-import com.capgemini.wsb.fitnesstracker.exception.api.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
+@Slf4j
 class UserController {
 
     private final UserServiceImpl userService;
@@ -21,26 +23,54 @@ class UserController {
     @GetMapping
     public List<UserDto> getAllUsers() {
         return userService.findAllUsers()
-                          .stream()
-                          .map(userMapper::toDto)
-                          .toList();
-    }
-    @GetMapping("/email")
-    public List<UserDto> getUserByEmail(@RequestParam String email) {
-        return userService.getUserByEmail(email)
-                .map(user -> List.of(userMapper.toDto(user)))
-                .orElseThrow(() -> new NotFoundException("User not found with email: " + email));
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 
+    @GetMapping("/{Id}")
+    public User getUser(@PathVariable Long Id) throws InterruptedException {
+        return userService.getUser(Id).get();
+    }
 
     @PostMapping
-    public User addUser(@RequestBody UserDto userDto) throws InterruptedException {
-
-        // Demonstracja how to use @RequestBody
-        System.out.println("User with e-mail: " + userDto.email() + "passed to the request");
-
-        // TODO: saveUser with Service and return User
-        return null;
+    public ResponseEntity<User> addUser(@RequestBody UserDto userDto) throws InterruptedException {
+        return ResponseEntity.status(201).body(userService.createUser(userMapper.toEntity(userDto)));
     }
 
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody UserDto newUserDto) {
+        UserDto oldEntityDto = userMapper.toDto(userService.getUser(userId).get());
+        User updatedEntity = userMapper.toEntity(oldEntityDto.mergeWith(newUserDto));
+        return userService.updateUser(updatedEntity.setId(userId));
+    }
+
+    @DeleteMapping("/{Id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long Id) throws InterruptedException {
+        userService.deleteUserById(Id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/email")
+    public UserDto getUserByEmail(@RequestParam String email) {
+        return userMapper.toDto(userService.getUserByEmail(email).get());
+    }
+
+    @GetMapping("/simple")
+    public List<SimpleUserDto> getSimpleUsers() {
+        return userService.findAllUsers()
+                .stream()
+                .map(user -> userMapper.toSimpleDto(user))
+                .toList();
+    }
+
+    @GetMapping("/older/{time}")
+    public List<UserDto> getOlderUsers(@PathVariable LocalDate time) {
+        List<UserDto> users = userService.findAllUsers()
+                .stream()
+                .filter(user -> user.getBirthdate().isAfter(time))
+                .map(userMapper::toDto)
+                .toList();
+        return users;
+    }
 }
