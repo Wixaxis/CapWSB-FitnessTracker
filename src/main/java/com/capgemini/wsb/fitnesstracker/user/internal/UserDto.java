b7ea8599c0d5acde33928ea3d668a01f73f1e2a3
Wiktor Rojecki <wixaxis@proton.me>
2024-10-20 15:54:34 +0200

    add mergeWith to UserDto

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserDto.java
@@ -6,7 +6,17 @@ import jakarta.annotation.Nullable;
 import java.time.LocalDate;
 
 record UserDto(@Nullable Long Id, String firstName, String lastName,
-               @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthdate,
-               String email) {
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthdate,
+        String email) {
+    public UserDto mergeWith(UserDto updatedUserDto) {
+        return new UserDto(Id(),
+        takeValid(updatedUserDto.firstName(), firstName),
+        takeValid(updatedUserDto.lastName(), lastName),
+        takeValid(updatedUserDto.birthdate(), birthdate),
+        takeValid(updatedUserDto.email(), email));
+    }
 
+    private static <T> T takeValid(T option1, T option2) {
+        return option1 == null ? option2 : option1;
+    }
 }
