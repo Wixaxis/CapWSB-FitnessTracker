2f30b8c2fbd2a8834b09907d2e155e84d6debb7c
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2024-11-09 18:27:11 +0100

    TrainingDto and implemented findAllTrainings (GET /v1/trainings)

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.capgemini.wsb.fitnesstracker.training.internal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.capgemini.wsb.fitnesstracker.training.api.Training;
+import com.capgemini.wsb.fitnesstracker.user.internal.UserDto;
+import com.capgemini.wsb.fitnesstracker.user.internal.UserMapper;
+
+/**
+ *
+ * @author oleko
+ */
+@Component
+class TrainingMapper {
+
+    private final UserMapper userMapper;
+
+    @Autowired
+    public TrainingMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public TrainingDto toDto(Training training){
+        UserDto userDto = userMapper.toDto(training.getUser());
+        return new TrainingDto(
+                training.getId(),
+                userDto,
+                training.getStartTime(),
+                training.getEndTime(),
+                training.getActivityType(),
+                training.getDistance(),
+                training.getAverageSpeed()
+        );
+    };
+
+
+
+}
