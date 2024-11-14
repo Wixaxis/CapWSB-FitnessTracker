7bc1852afb6084b826fdc05fd62626ac80363a88
Wiktor Rojecki <wixaxis@proton.me>
2024-11-14 05:44:36 +0100

    put -> createTraining endpoint

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -3,8 +3,11 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +58,19 @@ class TrainingController {
                             .stream()
                             .map(trainingMapper::toDto)
                             .toList();
+    /**
+     * Creates a new training session.
+     *
+     * @param trainingDto TrainingDto object containing training details
+     * @return A ResponseEntity containing the created Training object
+     */
+    @PostMapping
+    public ResponseEntity<TrainingDto> createTraining(@RequestBody TrainingDto trainingDto) {
+        return ResponseEntity.status(201)
+                .body(trainingMapper.toDto(trainingService.createTraining(trainingMapper.toEntity(trainingDto))));
+    }
+
+    /**
     }
 
     /**
